77a779aefb905311a93ad511a20aa08297ebc5cb
Ollie Treend <ollie@treend.uk>
2024-03-08 11:11:29 +0000

    Use headless Chrome in CI
    But full-fat Chrome when not in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,9 @@ require "rspec"
 require "capybara/rspec"
 
 Capybara.configure do |capybara|
-  capybara.default_driver = :selenium_chrome_headless
+  capybara.default_driver = if ENV["CI"]
+                              :selenium_chrome_headless
+                            else
+                              :selenium_chrome
+                            end
 end
