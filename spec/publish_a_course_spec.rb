183acdc29fee863feb6395b483d7b4ec75a2e9ed
Ollie Treend <ollie@treend.uk>
2024-03-08 12:02:31 +0000

    Keep creating a course

--- spec/publish_a_course_spec.rb
+++ spec/publish_a_course_spec.rb
@@ -9,5 +9,37 @@ RSpec.describe "Publish a course", type: :feature do
 
     choose "Primary"
     click_on "Continue"
+
+    choose "Primary with geography and history"
+    click_on "Continue"
+
+    choose "5 to 11"
+    click_on "Continue"
+
+    choose "PGCE with QTS"
+    click_on "Continue"
+
+    choose "Fee - no salary"
+    click_on "Continue"
+
+    choose "Full time"
+    click_on "Continue"
+
+    check "Avanti Fields School"
+    click_on "Continue"
+
+    choose "No"
+    click_on "Continue"
+
+    choose "As soon as the course is on Find - recommended"
+    click_on "Continue"
+
+    choose "March 2025" # TODO: this will break after March 2025
+    click_on "Continue"
+
+    click_on "Add course"
+
+    expect(page).to have_content("Your course has been created")
+    expect(page).to have_content("Primary with geography and history")
   end
 end
